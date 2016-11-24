15c9627bf1e70905f3ef5176de6304a5f301cbe0
Jon <jonr.web@gmail.com>
2016-11-24 21:49:11 +0000

    build basic ui

--- blackJack.js
+++ blackJack.js
@@ -6,37 +6,40 @@ window.addEventListener("ready", (function (doc) {
   class Game {
 
     constructor(options) {
-      this.currentPlayer = 1;
+      this.setConfig(options);
 
-      const config = gameSettings(options);
+//      console.log(this.config.players);
 
-      this.control = {
-        parent : doc.createElement('div')
+      //this.control = { parent : doc.createElement('div') };
+
+
+      this.cards = new Deck(this.config.deckCount);
+
+      //config.players.unshift('Dealer');
+      //this.currentPlayer = 1;
+      this.players = this.config.players.map((n) => {return new Player(n);});
+
+      this.control = new Ui(this);
+
+      console.log(this.players);
+
+    }
+
+    //game settings, with defaults
+    setConfig(opts) {
+      opts = opts || {};
+      this.config = {
+        tableElement : opts.hasOwnProperty('tableElement') ? opts.tableElement : '#jr_cardTable',
+        deckCount : opts.hasOwnProperty('deckCount') ? opts.deckCount : 6,
+        players : ['Dealer'].concat(
+          opts.hasOwnProperty('players') ? opts.players : ['player-1']
+        )
+
       };
-
-      config.players.unshift('Dealer');
-
-      this.cards = new Deck(config.deckCount);
-
-
-
-
-
-
     }
 
 
   }
-
-  //game settings, with defaults
-  function gameSettings(opts) {
-    return {
-      tableElement : opts.hasOwnProperty('tableElement') ? opts.tableElement : '#jr_cardTable',
-      deckCount : opts.hasOwnProperty('deckCount') ? opts.deckCount : 6,
-      players : opts.hasOwnProperty('players') ? opts.players : ['player']
-    };
-  }
-
 
   /* - deck object -------------------------------------------------------- */
   class Deck {
@@ -66,25 +69,112 @@ window.addEventListener("ready", (function (doc) {
 
 
   /* - player object ------------------------------------------------------ */
-  class player {
-    constructor(id,name) {
-
+  class Player {
+    constructor(name) {
+      this.name = name;
+      this.score = 0;
+      this.hand = [];
+      this.output = {};
+      this.hardAce = true;
     }
+
+    setOutput() {
+      this.output.title.innerHTML = this.name;
+      this.output.score.innerHTML = '0';
+    }
+
   }
 
   /* - ui object ---------------------------------------------------------- */
-  class ui {
-    constructor(elQuery) {
-      this.table = doc.querySelector(elQuery);
+  class Ui {
+    constructor(game) {
+      this.table = doc.querySelector(game.config.tableElement);
+
+      //the control input
+
+
+      //the outputs for each player
+      let outputs = game.players.map(this.setOutputs);
+
+      this.table.appendChild(this.setInput());
+      outputs.forEach((el) => {this.table.appendChild(el);});
+
+/*      for (player of game.players) {
+        this.
+      }*/
+
 
     }
+
+    setInput() {
+      let input = doc.createElement('div');
+      input.className = 'control-box';
+      ['hit', 'stand', 'restart'].forEach((name) => {
+        var newCtrl = document.createElement('button');
+        newCtrl.className = 'ctrl ctrl-' + name;
+        newCtrl.innerHTML = name;
+        newCtrl.addEventListener('click', gamePlay[name]);
+        input.appendChild(newCtrl);
+        //control[name] = newCtrl;
+      });
+
+      return input;
+    }
+
+
+
+    setOutputs(playerObj, index) {
+      let frame = doc.createElement('div');
+
+      frame.className = `player-frame player-${index}`;
+      //frame.style.top = (index == 0) ? '20px' : 'calc(100% - 150px)';
+      //out.cardPosY =
+      ['score', 'title', 'hand'].forEach((el) => {
+        let newEl = document.createElement('div');
+        newEl.className = el;
+        frame.appendChild(newEl);
+        playerObj.output[el] = newEl;
+      });
+
+      playerObj.setOutput();
+
+      return frame;
+    }
   }
+
 
   /* - score functions ---------------------------------------------------- */
 
 
   /* - gameplay functions ------------------------------------------------- */
 
+  function gamePlay() {
+    let out = {
+/*      output: {},
+      players: [],*/
 
-  return new Game();
+      hit: function () {
+
+        //game.players[game.currentPlayer].hit();
+      },
+      stand: function () {
+//        game.nextPlayer();
+//        if (game.currentPlayer == 0) {
+//           game.players[game.currentPlayer].autoPlay();
+//        }
+      },
+      restart: function () {
+//        game.newGame();
+      }
+    };
+
+    return out;
+  }
+
+
+  return new Game({
+    deckCount:1,
+    players: [
+      'Adam','Beth','Chris','Denise','Edward'
+    ]});
 }(document)));
