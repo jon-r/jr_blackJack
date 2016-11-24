d861fed8362a772a6794ccc93bfd2adbb69250c2
LVMA <LVMA@LVMAs-iMac.local>
2016-11-24 16:24:59 +0000

    adding classes

--- blackJack.js
+++ blackJack.js
@@ -1,19 +1,50 @@
 window.addEventListener("ready", (function (doc) {
   "use strict";
 
-  /* - gameplay functions ------------------------------------------------- */
+
+  /* - game object -------------------------------------------------------- */
+  class Game {
+
+    constructor(options) {
+      this.currentPlayer = 1;
+
+      const config = gameSettings(options);
+
+      this.control = {
+        parent : doc.createElement('div')
+      };
+
+      config.players.unshift('Dealer');
+
+      this.cards = new Deck(config.deckCount);
 
 
 
-  /* - score functions ---------------------------------------------------- */
 
 
+
+
+
+    }
+
+
+  }
+
+  //game settings, with defaults
+  function gameSettings(opts) {
+    return {
+      tableElement : opts.hasOwnProperty('tableElement') ? opts.tableElement : '#jr_cardTable',
+      deckCount : opts.hasOwnProperty('deckCount') ? opts.deckCount : 6,
+      players : opts.hasOwnProperty('players') ? opts.players : ['player']
+    };
+  }
 
 
   /* - deck object -------------------------------------------------------- */
-  class deck {
+  class Deck {
     constructor(decksCount) {
       this.cards = [];
+      let i, j, k;
 
       //decks
       for (i = 0; i < decksCount; i++) {
@@ -21,19 +52,18 @@ window.addEventListener("ready", (function (doc) {
         for (j = 0; j < 4; j++) {
           //values
           for (k = 1; k < 14; k++) {
-            deck.cards.push([k,j])
+            this.cards.push([k,j]);
           }
         }
       }
+    }
 
-      this.count =
-
+    //takes a random card out of the deck
+    drawOne() {
+      let rng = Math.floor(Math.random() * this.cards.length);
+      return this.cards.splice(rng,1)[0];
     }
   }
-
-
-
-  /* - game object -------------------------------------------------------- */
 
 
 
@@ -45,7 +75,18 @@ window.addEventListener("ready", (function (doc) {
   }
 
   /* - ui object ---------------------------------------------------------- */
+  class ui {
+    constructor(baseEl) {
+      this.table = baseEl;
+
+    }
+  }
+
+  /* - score functions ---------------------------------------------------- */
 
 
+  /* - gameplay functions ------------------------------------------------- */
 
+
+  return new Game();
 }(document)));
